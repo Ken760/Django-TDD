521a4533c73d014dc21508e51ece2b8557293946
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-09-12 13:12:54 +0300

    added unittest

--- autotest/functional_tests.py
+++ autotest/functional_tests.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
-browser = webdriver.Chrome(executable_path="driver./chromedriver")
+import unittest
 
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """тест нового посетителя"""
+
+    def setUp(self):
+        """установка"""
+        self.browser = webdriver.Chrome(executable_path="driver/chromedriver")
+
+    def tearDown(self):
+        """демонтаж"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """тест: можно начать список и получить его позже"""
+
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
