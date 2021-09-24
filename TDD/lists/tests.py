dc80770b9b2cffe15038fd2ec2bcd5de484049c7
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-09-24 18:48:32 +0300

    testing

--- TDD/lists/tests.py
+++ TDD/lists/tests.py
@@ -1,9 +1,15 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    '''тест на токсичность'''
 
-    def test_bad_maths(self):
-        '''тест: неправильные математические расчеты'''
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    """тест домашней страницы"""
+    def test_root_url_resolves_to_home_page_view(self):
+        """тест: корневой url преобразуется в представление
+        домашней страницы"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+
 
